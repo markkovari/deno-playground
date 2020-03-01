c0e37e7135f9cd6d68db19303ab2f4726cb5cc01
markkovari <kovarimarkofficial@gmail.com>
2020-03-01 23:44:04 +0100

    fix: docker environment

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # deno-playground
 
-if you don't want to make big mistakes run the app with docker-compose up
+if you don't want to make big mistakes run the app with docker-compose up or run in the background with the `-d` flag to run in detached mode
--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import { serve } from "https://deno.land/std@v0.35.0/http/server.ts";
+
+const s = serve({ port: 8000 });
+
+console.log("http://localhost:8000/");
+
+
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
+
